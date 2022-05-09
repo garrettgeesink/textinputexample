18cbbaad187342616679261688f01739a2b5f2f9
chatmansave@gmail.com <chatmansave@gmail.com>
2022-05-09 13:26:13 -0700

    added a button

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import emojiList from './emojiList.json'
 
 const App = () => {
   const [textInput, setTextInput] = useState('') // hook 
+  const [filterInput, setFilterEmoji] = useState('')
   return (
     <center>
       <Title />
-      <SearchInput onChange={
-        function(event) {
-          setTextInput(event.target.value)
-        }
-      } /> 
-      <SearchResults emojis={emojiList} textInput={textInput} />
+      <div>
+        <SearchInput onChange={function(event) {setTextInput(event.target.value)}} />  {/* Updates textInput every time you type a character */}
+        <button onClick={function() {setFilterEmoji(textInput)}}>submit</button> {/* uses textInput to call setFilterEmoji, which updates filter input */}
+      </div>
+      <SearchResults emojis={emojiList} textInput={filterInput} /> {/* filters the emojis using filterInput */}
     </center>
   )
 }
